d77664b9aab5cd6d7661b289ba2afadc3bbf9f3b
Qalb Hussain <qalbhussain94@hotmail.com>
2017-07-20 16:27:08 +0500

    remove unneccessary code

--- app/src/main/java/com/ubuntu/qalb/googlemap/MainActivity.java
+++ app/src/main/java/com/ubuntu/qalb/googlemap/MainActivity.java
@@ -50,15 +50,6 @@ public class MainActivity extends AppCompatActivity
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener {
 
-
-
-    //THSI IS MY FIREBASE BRANCH
-
-
-    //Request codes
-    private final static int PERMISSION = 0xc8;
-    private final static int LOCATION = 0x1;
-
     //Request codes
     private final static int PERMISSION_LOCATION_REQUEST = 0xc8;
     private final static int LOCATION_SETTING_REQUEST_CODE = 0x1;
